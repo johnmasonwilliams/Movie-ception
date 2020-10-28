53ddeec3d7b58b58d480ec252265108befcf07dd
masonwilliams6 <mwilliams8979@gmail.com>
2020-10-28 02:09:10 -0400

    Added login/signup redirect to user page

--- Application/js/firebase.js
+++ Application/js/firebase.js
@@ -42,7 +42,9 @@ function login(){
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function(user) {
+    window.location.href = "./user.html";
+  }).catch(function(error) {
 
     window.location.href = "./index.html";
 
@@ -61,7 +63,9 @@ function signup() {
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).then(function(user) {
+    window.location.href = "./user.html";
+  }).catch(function(error) {
 
     // Handle Errors here.
     var errorCode = error.code;
